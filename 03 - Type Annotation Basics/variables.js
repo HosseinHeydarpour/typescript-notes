ec01485bc64156c17dddf1ead8bab35cc89336ba
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-13 08:56:52 +0330

    004 Compiling TypeScript

---
+++ 03 - Type Annotation Basics/variables.js
@@ -0,0 +1,11 @@
+var movieTitle = "Game of thrones";
+movieTitle = "House";
+// movieTitle = 9; Has error
+// movieTitle.upper(); Has eeror it is not available
+movieTitle.toUpperCase();
+var numcatLives = 9;
+numcatLives += 1;
+// numcatLives = "Zero";
+var gameOver = false;
+gameOver = true;
+// gameOver = "False";
