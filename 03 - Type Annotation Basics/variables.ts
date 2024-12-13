5d41406b01dfa7110f0946419ce2428cf0a4445c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-13 08:51:00 +0330

    002 Working With Numbers & Booleans

--- 03 - Type Annotation Basics/variables.ts
+++ 03 - Type Annotation Basics/variables.ts
@@ -3,3 +3,11 @@ movieTitle = "House";
 // movieTitle = 9; Has error
 // movieTitle.upper(); Has eeror it is not available
 movieTitle.toUpperCase();
+
+let numcatLives: number = 9;
+numcatLives += 1;
+// numcatLives = "Zero";
+
+let gameOver: boolean = false;
+gameOver = true;
+// gameOver = "False";
