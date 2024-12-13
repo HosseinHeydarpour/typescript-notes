4a9ff0b3d5dfbd57bf7dce3e443f97f40fae8f47
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-13 08:47:51 +0330

    001 Our First Type Annotation

--- 02 - Installation & Setup/basics.ts
+++ 02 - Installation & Setup/basics.ts
@@ -3,4 +3,4 @@ console.log("hello");
 console.log("Good Bye");
 console.log(Math.round(5.44442644));
 
-let p: number = 3.1421;
+// let p: number = 3.1421;
---
+++ 03 - Type Annotation Basics/variables.ts
@@ -0,0 +1,5 @@
+let movieTitle: string = "Game of thrones";
+movieTitle = "House";
+// movieTitle = 9; Has error
+// movieTitle.upper(); Has eeror it is not available
+movieTitle.toUpperCase();
