9deab423926d7766c8f7ed1ed9e5b716d5f0dfc8
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-13 08:32:54 +0330

    004 How We Use TypeScript

---
+++ 02 - Installation & Setup/basics.js
@@ -0,0 +1,5 @@
+// .ts tells the editor that we are using typescript
+console.log("hello");
+console.log("Good Bye");
+console.log(Math.round(5.44442644));
+var p = 3.1421;
---
+++ 02 - Installation & Setup/basics.ts
@@ -0,0 +1,6 @@
+// .ts tells the editor that we are using typescript
+console.log("hello");
+console.log("Good Bye");
+console.log(Math.round(5.44442644));
+
+let p: number = 3.1421;
