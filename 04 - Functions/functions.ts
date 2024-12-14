f71d02f04df40a329a7dd6482b300a089d0bc1f3
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-14 18:17:29 +0330

    005 Return Type Annotations

--- 04 - Functions/functions.ts
+++ 04 - Functions/functions.ts
@@ -30,3 +30,26 @@ function greet(person: string = "stranger") {
 greet("John");
 greet();
 // greet(12312); has eeror it is either string or nothing
+
+// *****************************
+//  005 Return Type Annotations
+// *****************************
+
+function cube(num: number): number {
+  return num * num * num;
+}
+
+square(4);
+
+// Th ecase that the function can return multiple value types Union types
+function random(num: number): number | string {
+  if (Math.random() < 0.5) {
+    return num.toString();
+  } else {
+    return num;
+  }
+}
+
+const add = (x: number, y: number): number => {
+  return x + y;
+};
