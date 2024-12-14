4b7bc7c843101957fc18cd276d6b3a2b2f8f7e70
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-14 18:10:44 +0330

    004 Working With Default Parameters

--- 04 - Functions/functions.ts
+++ 04 - Functions/functions.ts
@@ -8,17 +8,25 @@ square(3);
 // square('asfas'); has error
 // square(true); has error
 
-function greet(person: string) {
-  // person * person Not allowed
-  return console.log(`Hi ${person}`);
-}
-
 const doSomething = (person: string, age: number, isFunny: boolean) => {};
 
 doSomething("Abba", 23, true);
 // doSomething("Abba", 23, 23232); This has error
 // doSomething("Abba", 23); 1 parameter is missing
 
-greet("John");
 // greet(2);
 // greet(true);
+
+// In the JS
+function multiply(num = 2) {
+  return num * 2;
+}
+
+function greet(person: string = "stranger") {
+  // person * person Not allowed
+  return console.log(`Hi ${person}`);
+}
+
+greet("John");
+greet();
+// greet(12312); has eeror it is either string or nothing
