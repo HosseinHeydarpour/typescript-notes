8145449bcf66fadf7066dcca3c50cf2d00ff5d06
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-15 19:28:04 +0330

    002 More Object Types

--- 05 - Object Types/object-types.ts
+++ 05 - Object Types/object-types.ts
@@ -9,3 +9,16 @@ function printName(person: { first: string; last: string }): void {
 }
 
 printName({ first: "Hossein", last: "Heydarpour" });
+
+// ******************************
+// * 002 More Object Types
+// ******************************
+
+let coordinate: { x: number; y: number } = { x: 22, y: 2 };
+
+function randomCoordinate(): { x: number; y: number } {
+  return {
+    x: Math.random(),
+    y: Math.random(),
+  };
+}
