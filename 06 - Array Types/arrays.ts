30f1b5a0c3bcd9d944de438cfd8aa63b455989bf
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-17 19:35:57 +0330

    002 More Array Syntax

--- 06 - Array Types/arrays.ts
+++ 06 - Array Types/arrays.ts
@@ -5,3 +5,25 @@ activeUsers.push("Steve");
 const ageList: number[] = [1, 2, 3];
 ageList[0] = 99;
 // ageList[0] = "ankcs";
+
+// **************************
+// * 002 More Array Syntax
+// **************************
+const bools: Array<boolean> = [];
+const bool2: boolean[] = [];
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const coords: Point[] = [];
+coords.push({
+  x: 23,
+  y: 8,
+});
+coords.push({
+  x: 25,
+  y: 6,
+});
+
