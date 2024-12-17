c02f993abe94ee4e5b31ccd59c0d2ace2158d8a0
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-17 19:23:29 +0330

    06 - Array Types

---
+++ 06 - Array Types/arrays.ts
@@ -0,0 +1,7 @@
+// const activeUsers: [] = []; // this is type empty array
+const activeUsers: string[] = ["Hossein"]; // type + []
+activeUsers.push("Steve");
+
+const ageList: number[] = [1, 2, 3];
+ageList[0] = 99;
+// ageList[0] = "ankcs";
