3060cce4c46a21f6a173dba606c0e1fab894241f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-18 20:04:48 +0330

    003 Union Types and Arrays

--- 07 - Union Types/union-types.ts
+++ 07 - Union Types/union-types.ts
@@ -6,27 +6,27 @@ let age: number | string = 12; // This is a little bit crazy
 age = 23;
 age = "24";
 
-type Point = {
-  x: number;
-  y: number;
-};
+// type Point = {
+//   x: number;
+//   y: number;
+// };
 
-type Loc = {
-  lat: number;
-  long: number;
-};
+// type Loc = {
+//   lat: number;
+//   long: number;
+// };
 
 //  Both are valid
 
-let coordinates: Point | Loc = {
-  x: 1,
-  y: 34,
-};
+// let coordinates: Point | Loc = {
+//   x: 1,
+//   y: 34,
+// };
 
-coordinates = {
-  lat: 312.222,
-  long: 2341.22,
-};
+// coordinates = {
+//   lat: 312.222,
+//   long: 2341.22,
+// };
 
 // ***********************************
 // * 002 Type Narrowing w Union Types
@@ -47,3 +47,31 @@ function calculateTax(price: number | string, tax: number): number {
   }
   return price * tax;
 }
+
+// ***********************************
+// * 003 Union Types and Arrays
+// ***********************************
+
+// const nums : number[] = [1,2,3];
+// const stuff: any[] = ['a',true,2,3,{}]
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type Loc = {
+  lat: number;
+  long: number;
+};
+
+// It is either an array with value of type number or string
+const stuffwring: number[] | string[] = [1, 2, 3];
+
+// It is an array of number or string
+const stuff: (number | string)[] = ["sd", 2, 3];
+
+// it is either a number or an array of strings
+const stuff3: number | string[] = ["2"];
+
+const coords: (Point | Loc)[] = [];
