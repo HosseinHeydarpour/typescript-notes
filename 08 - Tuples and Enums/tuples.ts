369e35a88786bc4cb0022c53dbb6e3a8ba6838f6
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-19 21:18:06 +0330

    001 Introducing Tuples

---
+++ 08 - Tuples and Enums/tuples.ts
@@ -0,0 +1,7 @@
+// **********************
+// * 001 Introducing Tuples
+// **********************
+const stuff4: (number | string)[] = ["as", 12, 3];
+
+let color: [number, number, number];
+color = [1, 2, 3];
