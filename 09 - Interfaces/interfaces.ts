16451e497693f4414e297fdc9e633ae0c48aad41
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2024-12-20 22:46:53 +0330

    002 Readonly and Optional Interface Properties

--- 09 - Interfaces/interfaces.ts
+++ 09 - Interfaces/interfaces.ts
@@ -20,3 +20,22 @@ const p2t: Point2 = {
   x: 23,
   y: 34,
 };
+
+/******************************* */
+// * 001 Introducing Interfaces
+/****************************** */
+interface Person {
+  readonly id: number;
+  first: string;
+  last: string;
+  nickname?: string;
+}
+
+const thomas: Person = {
+  id: 222,
+  first: "Thomas",
+  last: "Hardy",
+};
+
+thomas.first = "Thomas2";
+// thomas.id = 2233; This has error because we can only set readonly property when defining the object
