2088c0059aef3443adeb05cdb405d4d7e32ad505
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-23 19:18:35 +0330

    008 Strict Option

--- 10 - The TypeScript Compiler/src/index.ts
+++ 10 - The TypeScript Compiler/src/index.ts
@@ -18,3 +18,13 @@ const juniper: Chicked = {
   name: "Juniper",
   age: 1,
 };
+
+// if we set strict to false this wont give us any error but if we set it to true this will have an error
+// function doThing(thing) {
+//   console.log(thing);
+// }
+
+// This will have error if we set strictNullCheck to true and does not have error if we set it to false
+let users: string[] | null = ["addd", "sds"];
+users = null;
+// users = undefined;
