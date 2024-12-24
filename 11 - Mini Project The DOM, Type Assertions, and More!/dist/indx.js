ebfdf69fe505640d26407b5143c1d1c9aa88b296
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-24 19:34:33 +0330

    86 - Using a Service for CrossComponent Communication

--- 11 - Mini Project The DOM, Type Assertions, and More!/dist/indx.js
+++ 11 - Mini Project The DOM, Type Assertions, and More!/dist/indx.js
@@ -1,11 +1,15 @@
 "use strict";
 const btn = document.getElementById("btn"); // This is risky
+const input = document.getElementById("todoinput");
+btn?.addEventListener("click", (e) => {
+    input.value;
+});
 // btn?.addEventListener("click", (e) => {
 //   alert("Clicked");
 // });
-btn.addEventListener("click", (e) => {
-    alert("Clicked");
-});
-const mystery = "Hello World";
+// btn.addEventListener("click", (e) => {
+//   alert("Clicked");
+// });
+// const mystery: unknown = "Hello World";
 // const numChars = mystery.length; // This will have error as it its unknown type
-const numChars = mystery.length; //
+// const numChars = (mystery as string).length; //
--- 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
+++ 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
@@ -1,13 +1,18 @@
 const btn = document.getElementById("btn")!; // This is risky
+const input: HTMLInputElement = document.getElementById("todoinput")!;
+
+btn?.addEventListener("click", (e) => {
+  input.value;
+});
 
 // btn?.addEventListener("click", (e) => {
 //   alert("Clicked");
 // });
-btn.addEventListener("click", (e) => {
-  alert("Clicked");
-});
+// btn.addEventListener("click", (e) => {
+//   alert("Clicked");
+// });
 
-const mystery: unknown = "Hello World";
+// const mystery: unknown = "Hello World";
 
 // const numChars = mystery.length; // This will have error as it its unknown type
-const numChars = (mystery as string).length; //
+// const numChars = (mystery as string).length; //
