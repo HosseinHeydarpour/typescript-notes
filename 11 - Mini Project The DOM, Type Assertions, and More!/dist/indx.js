715e17e141e89e068ce9e5259deb9fac35396685
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-24 18:45:19 +0330

    005 Type Assertions

--- 11 - Mini Project The DOM, Type Assertions, and More!/dist/indx.js
+++ 11 - Mini Project The DOM, Type Assertions, and More!/dist/indx.js
@@ -6,3 +6,6 @@ const btn = document.getElementById("btn"); // This is risky
 btn.addEventListener("click", (e) => {
     alert("Clicked");
 });
+const mystery = "Hello World";
+// const numChars = mystery.length; // This will have error as it its unknown type
+const numChars = mystery.length; //
--- 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
+++ 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
@@ -6,3 +6,8 @@ const btn = document.getElementById("btn")!; // This is risky
 btn.addEventListener("click", (e) => {
   alert("Clicked");
 });
+
+const mystery: unknown = "Hello World";
+
+// const numChars = mystery.length; // This will have error as it its unknown type
+const numChars = (mystery as string).length; //
