5c5ca19defd68b0240ae7be5bc8f9000f67fdd22
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-25 18:15:31 +0330

    007 Working With Events

--- 10 - The TypeScript Compiler/dist/index.js
+++ 10 - The TypeScript Compiler/dist/index.js
@@ -1,4 +1,3 @@
-"use strict";
 const norma = {
     breed: "Silkieee",
     eggsPerWeek: 2,
@@ -11,3 +10,11 @@ const juniper = {
     name: "Juniper",
     age: 1,
 };
+// if we set strict to false this wont give us any error but if we set it to true this will have an error
+// function doThing(thing) {
+//   console.log(thing);
+// }
+// This will have error if we set strictNullCheck to true and does not have error if we set it to false
+let users = ["addd", "sds"];
+users = null;
+// users = undefined;
--- 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
+++ 11 - Mini Project The DOM, Type Assertions, and More!/src/indx.ts
@@ -2,10 +2,19 @@ const btn = document.getElementById("btn")!; // This is risky
 // const input = document.getElementById("todoinput")! as HTMLInputElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 
-btn.addEventListener("click", (e) => {
-  alert(input.value);
-  input.value = "";
-});
+const form = document.querySelector("form")!; // here because we are selecting HTLM elemnt type from ts know the type
+
+const handleSubmit = (e: SubmitEvent) => {
+  e.preventDefault();
+  console.log("SUBMITTED");
+};
+
+form.addEventListener("submit", handleSubmit);
+
+// btn.addEventListener("click", (e) => {
+//   alert(input.value);
+//   input.value = "";
+// });
 
 // btn?.addEventListener("click", (e) => {
 //   alert("Clicked");
