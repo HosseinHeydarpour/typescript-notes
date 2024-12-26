ab4cf3ff4b19905f37763f56d102ef0b46d01e87
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-26 19:30:38 +0330

    004 Class Fields

--- 12 - Classes/dist/index.js
+++ 12 - Classes/dist/index.js
@@ -1,20 +1,34 @@
 "use strict";
 
 class Player {
+  score = 0;
+  numLives = 10;
+
   constructor(first, last) {
     this.first = first;
     this.last = last;
+    // this.score = 0; old way
+    // this.numLives = 10; old way
   }
 
   taunt() {
     console.log("BOOYA");
   }
+
+  loseLife() {
+    this.numLives -= 1;
+  }
 }
 
 const player1 = new Player("Blue", "steele");
-player1.taunt();
-console.log(player1.last);
+// player1.taunt();
+// console.log(player1.last);
+// console.log(player1);
 
-const player2 = new Player("Red", "steele");
-player2.taunt();
-console.log(player2.first);
+console.log(player1.numLives);
+player1.loseLife();
+console.log(player1.numLives);
+
+// const player2 = new Player("Red", "steele");
+// player2.taunt();
+// console.log(player2.first);
