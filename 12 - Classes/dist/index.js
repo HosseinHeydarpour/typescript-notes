5e8ab2afef91210979dc5b4bdfb05c3d14981031
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-26 19:25:26 +0330

    003 Constructors

--- 12 - Classes/dist/index.js
+++ 12 - Classes/dist/index.js
@@ -1,14 +1,20 @@
 "use strict";
 
 class Player {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+  }
+
   taunt() {
     console.log("BOOYA");
   }
 }
 
-const player1 = new Player();
+const player1 = new Player("Blue", "steele");
 player1.taunt();
-console.log(player1);
+console.log(player1.last);
 
-const player2 = new Player();
+const player2 = new Player("Red", "steele");
 player2.taunt();
+console.log(player2.first);
