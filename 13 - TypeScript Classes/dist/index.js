aca87bab97aeb246f0c6943482dd32ff17ada561
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-27 09:01:02 +0330

    002 Class Fields In TypeScript

--- 13 - TypeScript Classes/dist/index.js
+++ 13 - TypeScript Classes/dist/index.js
@@ -1,8 +1,13 @@
 "use strict";
 class Player {
     constructor(first, last) {
+        // score: number;
+        this.score = 0;
         this.first = first;
         this.last = last;
+        // this.score = 0;
     }
 }
 const elton = new Player("Elton", "Steele");
+elton.score = 2;
+// elton.score = 'asca'; // This will have error
--- 13 - TypeScript Classes/src/index.ts
+++ 13 - TypeScript Classes/src/index.ts
@@ -1,11 +1,16 @@
 class Player {
   first: string;
   last: string;
+  // score: number;
+  score = 0;
 
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
+    // this.score = 0;
   }
 }
 
 const elton = new Player("Elton", "Steele");
+elton.score = 2;
+// elton.score = 'asca'; // This will have error
