c8e8f669c42c2db65a71262e15dbf41a246094de
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-27 09:39:20 +0330

    005 The private Modifier

--- 13 - TypeScript Classes/dist/index.js
+++ 13 - TypeScript Classes/dist/index.js
@@ -1,15 +1,17 @@
 "use strict";
 class Player {
     constructor(first, last) {
-        // score: number;
+        // #score: number; This is real JS
         this.score = 0;
         this.first = first;
         this.last = last;
         // this.score = 0;
+        this.secretMethod();
+    }
+    secretMethod() {
+        console.log("SECRETTTT!");
     }
 }
 const elton = new Player("Elton", "Steele");
-elton.score = 2;
-// elton.first = 'cvndskv';
-// elton.score = 'asca'; // This will have error
-// elton.first = "eltion"; // This will have error because  its i readnly
+// elton.score = 2; // This will have error because it is private
+// elton.secretMethod(); // Thiis will have error in TS bacuase it is private
--- 13 - TypeScript Classes/src/index.ts
+++ 13 - TypeScript Classes/src/index.ts
@@ -1,19 +1,21 @@
 class Player {
   public readonly first: string;
   public readonly last: string;
-  // score: number;
-  public score = 0;
+  // #score: number; This is real JS
+  private score = 0;
 
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
     // this.score = 0;
+    this.secretMethod();
+  }
+
+  private secretMethod() {
+    console.log("SECRETTTT!");
   }
 }
 
 const elton = new Player("Elton", "Steele");
-elton.score = 2;
-// elton.first = 'cvndskv';
-// elton.score = 'asca'; // This will have error
-
-// elton.first = "eltion"; // This will have error because  its i readnly
+// elton.score = 2; // This will have error because it is private
+// elton.secretMethod(); // Thiis will have error in TS bacuase it is private
