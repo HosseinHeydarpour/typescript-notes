8b86d238c34373726b66e7965734d3c0b8615920
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-28 18:26:42 +0330

    007 Getters and Setters

--- 13 - TypeScript Classes/dist/index.js
+++ 13 - TypeScript Classes/dist/index.js
@@ -16,16 +16,34 @@
 // }
 class Player {
     // private score = 0;
-    constructor(first, last, score) {
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
-        this.score = score;
+        this._score = _score;
     }
     secretMethod() {
         console.log("SECRETTTT!");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("score is negative");
+        }
+        this._score = newScore;
+    }
 }
 const elton = new Player("Elton", "Steele", 100);
 console.log(elton);
+console.log(elton.fullName);
 // elton.score = 2; // This will have error because it is private
 // elton.secretMethod(); // Thiis will have error in TS bacuase it is private
+// elton.fullName '0ada'; this will have error
+console.log(elton.score);
+elton.score = 99;
+// elton.score = -99; this will throw an error but Type script wont intercept this error
+// elton.score = 'cm'; This will have error too but typescript can intercept this
--- 13 - TypeScript Classes/src/index.ts
+++ 13 - TypeScript Classes/src/index.ts
@@ -20,17 +20,41 @@ class Player {
   // private score = 0;
 
   constructor(
-    public readonly first: string,
-    public readonly last: string,
-    private score: number
+    public first: string,
+    public last: string,
+    private _score: number
   ) {}
 
   private secretMethod() {
     console.log("SECRETTTT!");
   }
+
+  get fullName() {
+    return `${this.first} ${this.last}`;
+  }
+
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) {
+      throw new Error("score is negative");
+    }
+    this._score = newScore;
+  }
 }
 
 const elton = new Player("Elton", "Steele", 100);
 console.log(elton);
+console.log(elton.fullName);
 // elton.score = 2; // This will have error because it is private
 // elton.secretMethod(); // Thiis will have error in TS bacuase it is private
+
+// elton.fullName '0ada'; this will have error
+console.log(elton.score);
+
+elton.score = 99;
+// elton.score = -99; this will throw an error but Type script wont intercept this error
+
+// elton.score = 'cm'; This will have error too but typescript can intercept this
