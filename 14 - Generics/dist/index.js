afb750151931c5f360b9b4600bfa94ad33685b5c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-29 18:13:04 +0330

    001 Introducing Generics

---
+++ 14 - Generics/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// function doThing(thing: number | string): number | string {
+//   return thing;
+// }
+// const numbers: string[] = [];
+// This is a factory for arrays
+const numbers = [];
+const colors = [];
---
+++ 14 - Generics/src/index.ts
@@ -0,0 +1,8 @@
+// function doThing(thing: number | string): number | string {
+//   return thing;
+// }
+
+// const numbers: string[] = [];
+// This is a factory for arrays
+const numbers: Array<number> = [];
+const colors: Array<string> = [];
