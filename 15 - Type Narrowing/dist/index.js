9552d975325440e2210aa2cc819e6e6b0e708d3e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-31 19:08:50 +0330

    002 Truthiness Guards

--- 15 - Type Narrowing/dist/index.js
+++ 15 - Type Narrowing/dist/index.js
@@ -4,3 +4,24 @@ function tripe(value) {
         return value.repeat(3);
     return value * 3;
 }
+const el = document.getElementById("#idk");
+if (el) {
+    // Here type script knows for sure that the type is HTMLElement and it is not null
+    // el.
+}
+else {
+    // Here type script knows for sure that the type is null
+    // el
+}
+const printLetters = (word) => {
+    if (word) {
+        for (let char of word) {
+            console.log(char);
+        }
+    }
+    else {
+        // Here the type is string or null because typescript cannot tell for sure it is null or string because '' is still a string but it is falsy
+        word;
+        console.log("YOU DID NOT PASS IN A WORD");
+    }
+};
--- 15 - Type Narrowing/src/index.ts
+++ 15 - Type Narrowing/src/index.ts
@@ -2,3 +2,25 @@ function tripe(value: number | string): number | string {
   if (typeof value === "string") return value.repeat(3);
   return value * 3;
 }
+
+const el = document.getElementById("#idk");
+
+if (el) {
+  // Here type script knows for sure that the type is HTMLElement and it is not null
+  // el.
+} else {
+  // Here type script knows for sure that the type is null
+  // el
+}
+
+const printLetters = (word: string | null) => {
+  if (word) {
+    for (let char of word) {
+      console.log(char);
+    }
+  } else {
+    // Here the type is string or null because typescript cannot tell for sure it is null or string because '' is still a string but it is falsy
+    word;
+    console.log("YOU DID NOT PASS IN A WORD");
+  }
+};
